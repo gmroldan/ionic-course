21181fdbefaf36f3d2967dcd5d6a5b5a6aad8d22
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2019-07-14 22:17:11 -0300

    New pages module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
+  ],
+  exports: [
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
+  ]
 })
 export class PagesModule { }
