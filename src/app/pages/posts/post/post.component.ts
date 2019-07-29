60ff3d1a7bd020a8edf63d16a3f58857251788b7
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2019-07-29 11:09:58 -0300

    @Input example

---
+++ src/app/pages/posts/post/post.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  @Input() message;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/posts/posts.module.ts
+++ src/app/pages/posts/posts.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './posts.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PostsRoutingModule
   ],
-  declarations: [PostsComponent]
+  declarations: [PostsComponent, PostComponent]
 })
 export class PostsModule { }
