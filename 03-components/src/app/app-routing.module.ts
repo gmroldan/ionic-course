d56b7f06d1698f0486e88fec18f5bf594ce6ac08
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2019-07-29 13:13:25 -0300

    New Ionic app

---
+++ 03-components/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full' },   
+  { path: 'index', loadChildren: './pages/index/index.module#IndexPageModule' } 
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
