33547ae9ddb847bcefe0ae256519cf6cd83ac109
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2019-08-11 11:57:24 -0300

    Components - ion-alert - simple example

--- 03-components/src/app/pages/alert/alert.page.ts
+++ 03-components/src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Button Cancel');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: (blah) => {
+            console.log('Button Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
