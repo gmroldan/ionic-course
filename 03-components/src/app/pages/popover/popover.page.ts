3b47efb9b0413e9825b8eb26fad38abc84dbbf1d
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2019-08-14 01:25:04 -0300

    ion-popover part 1

---
+++ 03-components/src/app/pages/popover/popover.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopinfoComponent } from 'src/app/components/popinfo/popinfo.component';
+
+@Component({
+  selector: 'app-popover',
+  templateUrl: './popover.page.html',
+  styleUrls: ['./popover.page.scss'],
+})
+export class PopoverPage implements OnInit {
+
+  constructor(private popoverController: PopoverController) { }
+
+  ngOnInit() {
+  }
+
+  async showPop() {
+    const popover = await this.popoverController.create({
+      component: PopinfoComponent
+    });
+
+    await popover.present();
+  }
+
+}
