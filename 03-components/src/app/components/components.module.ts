783f053570979481f626eb363f435cfe697da50d
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2019-08-13 23:25:16 -0300

    ion-menu v1

--- 03-components/src/app/components/components.module.ts
+++ 03-components/src/app/components/components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule,
--- 03-components/src/app/pages/index/index.page.ts
+++ 03-components/src/app/pages/index/index.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-index',
@@ -80,9 +81,13 @@ export class IndexPage implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor(private menuController: MenuController) { }
 
   ngOnInit() {
+  }
+
+  toggleMenu() {
+    this.menuController.toggle();
   }
 
 }
